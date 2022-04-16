a7a330d0f39f7d2f8a35c2a1be5cba161b81aee4
Rodrigo <rl44610@hotmail.com>
2022-04-16 00:50:34 -0400

    Inicializacion del proyecto
    se visualiza un mapa

---
+++ mapa/src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import Map from "./components/Map.vue";
+
+export default {
+  name: "App",
+  components: {
+    Map,
+  },
+};
+
+</script>
+
+
+<template>
+
+
+<div id="ca">
+  <Map/>
+</div>
+  
+
+
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+  color: #2c3e50;
+  position: relative;
+  
+}
+.vue-map-container{
+  position: relative;
+  display: inline-block;
+  width: 100%;
+  height: 100vh;
+  background-color: #d03ca4;
+
+}
+</style>
