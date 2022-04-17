9886e0b1e0b8df303f5c0d9df9ca63dac1f21615
Rodrigo <rl44610@hotmail.com>
2022-04-17 00:12:30 -0400

    Ordenamiento de los archivos
    + componente MenuIzquierdo
    
    autores:
    Jose
    Luis
    Rodrigo

---
+++ mapabien/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+
+  <div id="contenedor">
+    <MenuIzquierda/>
+    <MapaGoogle/>
+
+  </div>
+  
+
+ 
+
+
+</template>
+
+<script>
+import MapaGoogle from "./components/MapaGoogle.vue";
+import MenuIzquierda from "./components/MenuIzquierda.vue";
+
+
+export default {
+  name: "App",
+  components: {
+    MapaGoogle,
+    MenuIzquierda
+},
+};
+
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+  color: #2c3e50;
+  
+  
+}
+.vue-map-container{
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  bottom:  1px;
+  
+}
+
+#contenedor{
+
+  
+}
+</style>
---
+++ mapabien/src/components/MenuIzquierda.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="menuIzquierda" align="center">sadsad</div>
+
+
+</template>
+
+<script>
+export default {
+
+    data(){
+        return{
+
+           
+        }
+        
+
+    }
+
+}
+</script>
+
+<style scoped>
+
+ #menuIzquierda{
+    background-color: rgb(62, 0, 149);
+     
+    height: 100vh;
+    width: 10%;
+    position: relative;
+    color: aliceblue;
+
+    border-radius: 1%;
+    
+    z-index: 5;
+ }
+
+
+</style>
