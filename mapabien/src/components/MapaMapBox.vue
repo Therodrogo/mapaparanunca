5ec63367d4ddc84c0a7cf539bee5b45929eb2669
José Reveco <offjose@gmail.com>
2022-04-19 11:42:26 -0400

    LineasMagicas
    Lineas pueden aparecer cuando haces click

--- mapabien/src/components/MapaMapBox.vue
+++ mapabien/src/components/MapaMapBox.vue
@@ -41,19 +41,50 @@ export default {
         }
         }
         });
-      map.addLayer({
-      'id': 'route',
-      'type': 'line',
-      'source': 'route',
-      'layout': {
-      'line-join': 'round',
-      'line-cap': 'round'
-      },
-      'paint': {
-      'line-color': '#FFFF00',
-      'line-width': 8
-      }
+                
+        map.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'route',
+        'layout': {
+        'line-join': 'round',
+        'line-cap': 'round'
+        },
+        'paint': {
+        'line-color': '#FFFF00',
+        'line-width': 7
+        }
       });
+
+         map.addSource('route2', {
+        'type': 'geojson',
+        'data': {
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+        'type': 'LineString',
+        'coordinates': [
+        [-71.230,-35.003],
+        [-71.230529, -35.001914]
+        ]
+        }
+        }
+        });
+                
+        map.addLayer({
+        'id': 'route2',
+        'type': 'line',
+        'source': 'route2',
+        'layout': {
+        'line-join': 'round',
+        'line-cap': 'round'
+        },
+        'paint': {
+        'line-color': '#FFFF00',
+        'line-width': 7
+        }
+      });
+
 });
 
         var el = document.createElement('div');
@@ -62,25 +93,74 @@ export default {
         el.style.width = '50px';
         el.style.height = '50px';
         el.style.backgroundSize = '100%';
-
+    //  const algo = map.getSource('route2');
+        
 
         const marker1 = new mapboxgl.Marker(el)
         marker1.getElement().addEventListener('click', () => {
           
           map.zoomTo(20.01,[-71.230,-35.003] );
-          
+          map.removeLayer('route2');
+          map.removeSource('route2');
         });
+
+       
         marker1.setLngLat([-71.230,-35.003]);
         marker1.addTo(map);
         marker1.setDraggable(true);
         //marker1.style.backgroundImage = ''
 
       const marker2 = new mapboxgl.Marker()
-             
-
       marker2.setLngLat([-71.22929, -35.001566]);
       //EL ADDTO MAP DEBE ESTAR AL FINAL O NO CORRE XD
+
+      //MARKER DE ENTRADA MALA
+      marker2.getElement().addEventListener('click', () => {
+
+        //CREAR LINEA AMARILLA DENUEVO
+          map.zoomTo(20.01,[-71.230,-35.003] );
+       
+          map.addSource('route2', {
+        'type': 'geojson',
+        'data': {
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+        'type': 'LineString',
+        'coordinates': [
+        [-71.230,-35.003],
+        [-71.230529, -35.001914]
+        ]
+        }
+        }
+        });
+                
+        map.addLayer({
+        'id': 'route2',
+        'type': 'line',
+        'source': 'route2',
+        'layout': {
+        'line-join': 'round',
+        'line-cap': 'round'
+        },
+        'paint': {
+        'line-color': '#FFFF00',
+        'line-width': 7
+        }
+      });
+        });
+        marker2.setDraggable(true);
       marker2.addTo(map);
+
+      //Entrada
+      const marker3 = new mapboxgl.Marker()
+      marker3.setLngLat([-71.229743, -35.001366]);
+      //marker3.setHTML("ENTRADA");
+      //EL ADDTO MAP DEBE ESTAR AL FINAL O NO CORRE XD
+      marker3.addTo(map);
+      
+
+//  -71.230529, -35.001914 Coordenadas para crear camino
 
      
 
@@ -91,10 +171,15 @@ export default {
         map.setMaxZoom(18);
       
 
-        const popup = new mapboxgl.Popup({ closeOnClick: false })
-        popup.setLngLat([-71.230,-35.003])
-        popup.setHTML('<h1>hola</h1>')
-        marker1.setPopup(popup);
+        // const popup = new mapboxgl.Popup({ closeOnClick: false })
+        // popup.setLngLat([-71.230,-35.003])
+        // popup.setHTML('<h1>hola</h1>')
+        // marker1.setPopup(popup);
+
+      
+
+
+        
   
     });
     return {};
